cd6bb52d989f95f0f49c5cd22c03cdc6a7aed1c3
youenchene <youen.chene@gadz.org>
2015-12-07 23:26:14 +0100

    fix: csv generation
    UTF16 not comaptible with impala/hive

--- src/main/java/io/saagie/demo/extract/senseit/Main.java
+++ src/main/java/io/saagie/demo/extract/senseit/Main.java
@@ -1,15 +1,18 @@
 package io.saagie.demo.extract.senseit;
 
 
+import au.com.bytecode.opencsv.CSVWriter;
 import com.github.kevinsawicki.http.HttpRequest;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import io.saagie.demo.extract.senseit.dto.History;
 import io.saagie.demo.extract.senseit.dto.SenseIT;
+import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.log4j.Logger;
 
+import java.io.*;
 import java.util.Date;
 import java.util.Iterator;
 
@@ -21,6 +24,7 @@ public class Main {
 
 
    private static final Logger logger = Logger.getLogger(Main.class);
+   public static final char SEP = ';';
 
    public static void main(String[] args) throws Exception {
       if (args.length<4) {
@@ -45,21 +49,16 @@ public class Main {
       String jsondata=new Gson().toJson(senseIT.data);
 
 
-      logger.info("Building CSV...");
-      String csv="device;sensor;sensor_type;date;data;value\n";
+      String csv = buildCSV(deviceid, sensorid, senseIT);
 
-      Iterator<History> it_his=senseIT.data.history.iterator();
-      while(it_his.hasNext()) {
-         History his=it_his.next();
-         csv+=deviceid+";"+sensorid+";"+senseIT.data.sensor_type+";"+his.date+";"+his.data+";"+his.getDataInDouble()+"\n";
-      }
-      logger.debug("CSV :"+csv);
+      ByteArrayOutputStream csv_os = buildCSV2(deviceid, sensorid, senseIT);
 
       // Create Directories
       logger.debug("Create directory");
       FileSystem fs = HdfsUtils.getFileSystemFromUri(hdfsuri);
       Path directoryraw = HdfsUtils.createDirectory(fs, "/iot/sigfox/senseit/raw");
       Path directorycsv = HdfsUtils.createDirectory(fs, "/iot/sigfox/senseit/csv");
+      Path directorycsv2 = HdfsUtils.createDirectory(fs, "/iot/sigfox/senseit/csv2");
 
 
       String filename=deviceid+"-"+sensorid+"-"+senseIT.data.sensor_type+"-"+(new Date()).getTime();
@@ -70,7 +69,8 @@ public class Main {
 
       logger.info("Import done of raw file : "+filename+".json");
 
-      HdfsUtils.createFile(fs, directorycsv, filename+".csv", csv);
+      HdfsUtils.createFile(fs, directorycsv, filename+".csv", csv.getBytes());
+      HdfsUtils.createFile(fs, directorycsv, filename+".csv", csv_os.toByteArray());
 
       logger.info("Import done of csv file : "+filename+".csv");
 
@@ -78,5 +78,33 @@ public class Main {
       logger.info("Done.");
 
 
+   }
+
+   private static String buildCSV(String deviceid, String sensorid, SenseIT senseIT) {
+      logger.info("Building CSV...");
+      String csv="device;sensor;sensor_type;date;data;value\n";
+
+      Iterator<History> it_his=senseIT.data.history.iterator();
+      while(it_his.hasNext()) {
+         History his=it_his.next();
+         csv+=deviceid+ SEP +sensorid+ SEP +senseIT.data.sensor_type+ SEP +his.date+ SEP +his.data+ SEP +his.getDataInDouble()+"\n";
+      }
+      return csv;
+   }
+
+   private static ByteArrayOutputStream buildCSV2(String deviceid, String sensorid, SenseIT senseIT) throws IOException {
+      logger.info("Building CSV...");
+
+      ByteArrayOutputStream os = new ByteArrayOutputStream();
+      CSVWriter writer = new CSVWriter(new OutputStreamWriter(os,"UTF-8"));
+      writer.writeNext(new String[]{"device","sensor","sensor_type","date","data","value"});
+
+      Iterator<History> it_his=senseIT.data.history.iterator();
+      while(it_his.hasNext()) {
+         History his=it_his.next();
+         writer.writeNext(new String[]{deviceid,sensorid,senseIT.data.sensor_type,his.date,his.data,his.getDataInDouble().toString()});
+      }
+      writer.flush();
+      return os;
    }
 }
